2c1661c22887b4b2d68543a0802196466674b2ca
birukbelaytafese <birukbelay175@gmail.com>
2026-05-12 14:47:26 -0500

    Finished vehicle inheritance commit

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -4,34 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Create Moped object
-        Moped slowRide = new Moped();
-        slowRide.setColor("Red");
-        slowRide.setFuelCapacity(5);
+        // Moped object
+        Moped slowRide = new Moped("Red", 2, 50, 5);
 
-        // Create Car object
-        Car familyCar = new Car();
-        familyCar.setColor("Black");
-        familyCar.setNumberOfPassengers(5);
+        // Car object
+        Car familyCar = new Car("Black", 5, 500, 15);
 
-        // Create SemiTruck object
-        SemiTruck bigTruck = new SemiTruck();
-        bigTruck.setCargoCapacity(10000);
+        // SemiTruck object
+        SemiTruck bigTruck = new SemiTruck("Blue", 2, 10000, 300);
 
-        // Create Hovercraft object
-        Hovercraft hover = new Hovercraft();
-        hover.setColor("Silver");
+        // Hovercraft object
+        Hovercraft hover = new Hovercraft("Silver", 8, 2000, 100);
 
         // Print values
         System.out.println("Moped Color: " + slowRide.getColor());
 
-        System.out.println("Car Passengers: " +
-                familyCar.getNumberOfPassengers());
+        System.out.println("Car Passengers: "
+                + familyCar.getNumberOfPassengers());
 
-        System.out.println("Truck Cargo Capacity: " +
-                bigTruck.getCargoCapacity());
+        System.out.println("Truck Cargo Capacity: "
+                + bigTruck.getCargoCapacity());
 
-        System.out.println("Hovercraft Color: " +
-                hover.getColor());
+        System.out.println("Hovercraft Fuel Capacity: "
+                + hover.getFuelCapacity());
     }
 }
